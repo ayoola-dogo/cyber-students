09d7634a7b2857021b7a739ca6a713bcb0afe9bb
ayoola-dogo <pythonworkforay@gmail.com>
2025-05-05 03:42:14 +0100

    add AES-256 secret key generation

--- api/conf.py
+++ api/conf.py
@@ -1,3 +1,5 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
 PORT = 4000
 
 MONGODB_HOST = {
@@ -8,3 +10,12 @@ MONGODB_HOST = {
 MONGODB_DBNAME = 'cyberStudents'
 
 WORKERS = 32
+
+# Cryptographic configuration
+# In a production environment, these would be managed by a secrets management tool
+PASSWORD_HASH_ITERATIONS = 700000
+PASSWORD_HASH_LENGTH = 32  # Length of the hash value in bytes
+
+
+# Generate AES-256 secret key (256-bit key)
+AES_KEY = AESGCM.generate_key(bit_length=256)
