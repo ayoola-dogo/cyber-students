14c69e6c74983c9099d40bae3757d9ebc58dedfc
ayoola-dogo <pythonworkforay@gmail.com>
2025-05-15 14:14:34 +0100

    update registration with other fields and fix user profile test

--- test/registration.py
+++ test/registration.py
@@ -1,29 +1,37 @@
 from json import dumps
 from tornado.escape import json_decode
-from tornado.ioloop import IOLoop
 from tornado.web import Application
 
 from api.handlers.registration import RegistrationHandler
 
 from .base import BaseTest
 
-import urllib.parse
 
 class RegistrationHandlerTest(BaseTest):
 
     @classmethod
-    def setUpClass(self):
-        self.my_app = Application([(r'/registration', RegistrationHandler)])
+    def setUpClass(cls):
+        cls.my_app = Application([(r'/registration', RegistrationHandler)])
         super().setUpClass()
 
     def test_registration(self):
         email = 'test@test.com'
         display_name = 'testDisplayName'
+        full_name = 'Test User Full Name'
+        address = '123 Test Street, Test City'
+        dob = '1990-01-15'
+        phone_number = '1234567890'
+        disabilities = ['Hearing Impairment', 'Visual Impairment']
 
         body = {
-          'email': email,
-          'password': 'testPassword',
-          'displayName': display_name
+            'email': email,
+            'password': 'testPassword',
+            'displayName': display_name,
+            'full_name': full_name,
+            'address': address,
+            'dateOfBirth': dob,
+            'phone_number': phone_number,
+            'disabilities': disabilities
         }
 
         response = self.fetch('/registration', method='POST', body=dumps(body))
--- test/user.py
+++ test/user.py
@@ -1,5 +1,6 @@
+from api.crypto_utils import hash_password, encrypt_personal_data
 from json import dumps
-from tornado.escape import json_decode, utf8
+from tornado.escape import json_decode
 from tornado.gen import coroutine
 from tornado.httputil import HTTPHeaders
 from tornado.ioloop import IOLoop
@@ -9,21 +10,39 @@ from api.handlers.user import UserHandler
 
 from .base import BaseTest
 
-import urllib.parse
 
 class UserHandlerTest(BaseTest):
 
     @classmethod
-    def setUpClass(self):
-        self.my_app = Application([(r'/user', UserHandler)])
+    def setUpClass(cls):
+        cls.my_app = Application([(r'/user', UserHandler)])
         super().setUpClass()
 
     @coroutine
     def register(self):
+        self.personal_data = {
+            'displayName': self.display_name,
+            'fullName': self.full_name,
+            'address': self.address,
+            'dateOfBirth': self.doa,
+            'phoneNumber': self.phone_number,
+            'disabilities': self.disabilities,
+        }
+
+        # Hash the password for testing
+        password_data = hash_password(self.password)
+
+        # Encrypt the display name
+        user_encrypted_data = encrypt_personal_data(
+            dumps(self.personal_data)
+        )
+
         yield self.get_app().db.users.insert_one({
             'email': self.email,
-            'password': self.password,
-            'displayName': self.display_name
+            'personal_data': user_encrypted_data['encrypted_data'],
+            'personal_data_iv': user_encrypted_data['nonce'],
+            'salt': password_data['salt'],
+            'password': password_data['hash']
         })
 
     @coroutine
@@ -41,6 +60,15 @@ class UserHandlerTest(BaseTest):
         self.password = 'testPassword'
         self.display_name = 'testDisplayName'
         self.token = 'testToken'
+        self.full_name = 'Test DisplayName'
+        self.address = '435 Highway Street, Maryland'
+        self.doa = '01/05/1983'
+        self.phone_number = '1234567'
+        self.disabilities = [
+            'Dyslexia',
+            'Hearing Impairment',
+            'Visual Impairment',
+        ]
 
         IOLoop.current().run_sync(self.register)
         IOLoop.current().run_sync(self.login)
@@ -54,6 +82,12 @@ class UserHandlerTest(BaseTest):
         body_2 = json_decode(response.body)
         self.assertEqual(self.email, body_2['email'])
         self.assertEqual(self.display_name, body_2['displayName'])
+        self.assertEqual(self.full_name, body_2['fullName'])
+        self.assertEqual(self.address, body_2['address'])
+        self.assertEqual(self.doa, body_2['dateOfBirth'])
+        self.assertEqual(self.phone_number, body_2['phoneNumber'])
+        self.assertEqual(self.disabilities, body_2['disabilities'])
+
 
     def test_user_without_token(self):
         response = self.fetch('/user')
